198ad1324b260579d4ddba736df96c5dbfa77872
Ramon Vitor <vitorramon@hotmail.com>
2021-08-03 18:00:55 -0300

    Corrigindo requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
