0552cd9403a0ddc50b0bb3b8169061df0ffb4f7d
Ramon Vitor <vitorramon@hotmail.com>
2021-08-04 15:46:30 -0300

    Requisito 12

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
           <Link to='/'> Home </Link>
           <Link to='/about'> About </Link>
           <Link to='/users'> Users </Link>
+          <Link to='/strict-access'> Strict Acess </Link>
         </div>
         <div>
           <Switch>
